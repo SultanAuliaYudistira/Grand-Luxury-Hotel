e7422af515faf929aabe59f3e53cc05defd6c671
ihsannur23-code <ihsannur23@if.unjani.ac.id>
2026-01-06 12:31:53 +0700

    Update User.php

--- User.php
+++ User.php
@@ -1,31 +1,21 @@
-<?php
+<?php<?php
 
 class User
 {
-    // fungsi nyata: ambil user/admin berdasarkan username
-    public function getByUsername($username, $table = 'tbl_admin')
+    public $id_user;
+    public $nama;
+    public $email;
+    public $password;
+
+    public function login()
     {
-        require __DIR__ . '/../../config/koneksi.php';
-
-        $query = mysqli_query(
-            $koneksi,
-            "SELECT * FROM {$table} WHERE username='$username'"
-        );
-
-        return mysqli_fetch_assoc($query);
+        return true;
     }
 
-    // fungsi nyata: simpan user baru (register)
-    public function registerUser($data)
+    public function register()
     {
-        require __DIR__ . '/../../config/koneksi.php';
-
-        $password = password_hash($data['password'], PASSWORD_DEFAULT);
-
-        return mysqli_query(
-            $koneksi,
-            "INSERT INTO tbl_user (username, password)
-             VALUES ('{$data['username']}', '$password')"
-        );
+        return true;
     }
 }
+
+
